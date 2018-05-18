0a98c21837479efde64edef62713d0b2eacb192a
young <2461927976@qq.com>
2018-05-18 13:18:15 +0800

    增加一个员工登录

--- nodeLearn/day5/03.js
+++ nodeLearn/day5/03.js
@@ -19,6 +19,36 @@ app.get("/",function(req,res,next){
 	res.render("index");
 });
 
+app.get("/du",function(req,res,next){
+
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields) {
+        //写入数据库
+        db.query2 = function (sql,callback) {
+            if (!sql){
+                callback();
+                return;
+            }
+            pool.query(sql,function (err,result) {
+                if (err){
+                    console.log(err);
+                    return;
+                }
+                callback(null,result);
+            })
+        };
+        var sql = "select * from liuyan";
+        db.query2(sql,function(err,result){
+            if (err) {
+                res.send({"result":-1});
+                return;
+            }
+            res.json({"result":result});
+        });
+    });
+})
+
 app.post("/tijiao",function(req,res,next){
 	var form = new formidable.IncomingForm();
 
@@ -38,24 +68,15 @@ app.post("/tijiao",function(req,res,next){
             })
         };
 
-		var sql1 = "select * from liuyan";
-		db.query1(sql1,function(err,result1){
-			// if (err) {
-			// 	res.json("-1");
-			// }
-			// res.json("1");
-			// console.log(result1);
-			// return;
-		});
-
+		
 
         var addsql = 'insert into liuyan(姓名,留言) values(' + '\"' + fields.xingming+ '\"' + ',' + '\"' +fields.liuyan +'\"' + ')';
         db.query1(addsql,function(err){
             if (err) {
-                res.json("-1");
+                res.send({"result":-1});
                 return;
             }
-            res.json("1");
+            res.json({"result":1});
         });
 
 		console.log("收到请求了"+fields.xingming+fields.liuyan);
