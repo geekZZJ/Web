dd9e3c3a849ccb237f21cb22fc1aecdfdfeb1082
young <2461927976@qq.com>
2018-08-03 11:10:37 +0800

    php初学

---
+++ php/basis/01.php
@@ -0,0 +1,20 @@
+<?php 
+	header("Content-type:text/html;charset=utf-8");
+	if ($con = mysql_connect('localhost','root','')) {
+		echo "连接成功";
+	}else{
+		echo "连接失败";
+	}
+	if (mysql_select_db('test')) {
+		echo "选择数据库成功";
+	}else{
+		echo "选择数据库失败";
+	}
+	if (mysql_query('insert into stu(sno,sname) values(3,"asdf")')) {
+		echo "插入成功";
+	}else{
+		echo mysql_error();
+		echo "插入失败";
+	}
+	mysql_close($con);
+ ?>
