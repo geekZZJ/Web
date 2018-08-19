0788fad8670f934522e4d5fe82d9cf2381dea4a7
young <2461927976@qq.com>
2018-08-19 11:23:25 +0800

    更新

--- php/basis/article/admin/article.del.handle.php
+++ php/basis/article/admin/article.del.handle.php
@@ -5,3 +5,10 @@
  * Date: 2018/8/18
  * Time: 19:16
  */
+require_once ('../connect.php');
+$deleteSql = 'delete from article where id = 2';
+if (mysqli_query($con,$deleteSql)){
+    echo "<script>alert('删除文章成功');</script>";
+}else{
+    echo "<script>alert('删除文章失败');</script>";
+}
