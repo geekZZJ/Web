f0298afd6098b11f99de6131d1ad725b60c79e5c
young <2461927976@qq.com>
2018-08-22 21:22:10 +0800

    监测mysqli是否开启

--- php/mysqli/01.php
+++ php/mysqli/01.php
@@ -6,3 +6,11 @@
  * Time: 21:12
  */
 var_dump(extension_loaded('mysqli'));
+var_dump(extension_loaded('curl'));
+echo  '<hr/>';
+//监测函数是否存在
+var_dump(function_exists('mysqli_connect'));
+echo '<hr/>';
+//得到当前已经开启的拓展
+print_r(get_loaded_extensions());
+echo '<hr/>';
