f9deddc03b45a605532f8bfb31179d5667a09ba8
young <2461927976@qq.com>
2018-08-26 16:36:27 +0800

    vue配合bootstrap

---
+++ vuejs/day7/bootstrap-demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <button class="btn btn-default">普通按钮</button>
+    <button class="btn btn-danger">普通按钮</button>
+    <div class="row">
+      <div class="col-sm-8">8</div>
+      <div class="col-sm-4">4</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
