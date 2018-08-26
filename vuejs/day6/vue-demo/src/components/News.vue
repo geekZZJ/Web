e17ab39fc88cb20a99108ac6d2e2086e3a5da85e
young <2461927976@qq.com>
2018-08-26 15:56:34 +0800

    vue路由及动画

---
+++ vuejs/day6/vue-demo/src/components/News.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="news">
+		<h3>我们新闻页</h3>
+		<ul>
+			<li v-for="(val,index) in news">{{val}}</li>
+		</ul>	
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		name:'news',
+		data(){
+			return {
+				news:['123456','4567612745','33333333']
+			}
+		}
+	}
+</script>
