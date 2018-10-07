d4eefe6841f75431cae18fab4545029b35c334f3
young <2461927976@qq.com>
2018-10-07 20:39:54 +0800

    使用koa框架

---
+++ koa2/day1/cb-promise.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+// fs.readFile('./package.json',(err,data
+// )=>{
+//     if (err) return console.log(err)
+//
+//     data = JSON.parse(data)
+//     console.log(data.name)
+// })
+
+function readFileAsync(path) {
+    return new Promise((resolve,reject)=>{
+        fs.readFile(path,(err,data
+        )=>{
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+
+readFileAsync('./package.json')
+.then(data=>{
+    data = JSON.parse(data)
+    console.log(data.name)
+})
+.catch(err=>{
+    console.log(err)
+})
