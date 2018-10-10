3bb179d7d2f5a0ca2e866e00f123001cf1f0dfb7
young <2461927976@qq.com>
2018-10-10 20:21:42 +0800

    koa-logger

--- koa2/day3/index.js
+++ koa2/day3/index.js
@@ -1,8 +1,34 @@
 const Koa = require('koa')
+const logger = require('koa-logger')
 const app = new Koa()
 
-app.use(async(ctx,next)=>{
-    ctx.body = 'Hi Luke'
-})
+const mid1 = async(ctx,next)=>{
+    ctx.type = 'text/html;charset=utf-8'
+    await next()
+}
+const mid2 = async(ctx,next)=>{
+    ctx.body = 'Hi '
+    await next()
+}
+const mid3 = async(ctx,next)=>{
+    ctx.body = ctx.body + 'Luke'
+    await next()
+}
+
+// app.use(async(ctx,next)=>{
+//     // console.log(ctx.href)
+//     // console.log(ctx.path)
+//     // console.log(ctx.url)
+//     // console.log(ctx.method)
+//     // console.log(ctx)
+//     ctx.type = 'text/html;charset=utf-8'
+//     ctx.body = 'Hi Luke'
+// })
+
+app.use(logger())
+
+app.use(mid1)
+app.use(mid2)
+app.use(mid3)
 
 app.listen(2333)
