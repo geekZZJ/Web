1b3882e813ae9d2f52148d9fce9cc833e1badb18
young <2461927976@qq.com>
2018-10-12 11:40:37 +0800

    vuex

---
+++ vuejs/day9/vuex-demo/src/store.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+var state = {
+	count:10
+};
+
+const mutations={
+	increment(state){
+		state.count++
+	}
+}
+
+const actions={
+	increment:({commit})=>{
+		commit('increment')
+	}
+}
+
+const getters={
+	count(state){
+		return state.count;
+	}
+}
+
+export default new Vuex.Store({
+	actions,
+	mutations,
+	state,
+	getters
+})
