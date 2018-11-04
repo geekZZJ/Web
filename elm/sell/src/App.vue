129ae89ab99a92ace6778848e35c8654f35dff4c
young <2461927976@qq.com>
2018-11-04 14:19:21 +0800

    update

--- elm/sell/src/App.vue
+++ elm/sell/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
+import header from './components/header/header'
 export default {
-  name: 'App'
+  comments: {
+    'v-header': header
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
