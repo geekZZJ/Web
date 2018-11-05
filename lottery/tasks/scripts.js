b74aa4830774dcf1ba7afad1f0b61f0e8d866e63
young <2461927976@qq.com>
2018-11-05 13:31:36 +0800

    创建模板，服务任务模板

--- lottery/tasks/scripts.js
+++ lottery/tasks/scripts.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp';
-import gulpif from 'gulp-if'
+import gulpif from 'gulp-if';
 import concat from 'gulp-concat';
 import webpack from 'webpack';
 import gulpWebpack from 'webpack-stream';
