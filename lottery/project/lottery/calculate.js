4759eb267dc68e29895826b7f4a43a38d317bf97
young <2461927976@qq.com>
2018-11-06 09:39:32 +0800

    创建接口模块上

--- lottery/project/lottery/calculate.js
+++ lottery/project/lottery/calculate.js
@@ -1,62 +1,36 @@
-import $ from 'jquery';
-class Interface {
-    getOmit(issue){
-        let self = this;
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url:'/get/omit',
-                data:{
-                    issue:issue
-                },
-                dataType:'json',
-                success:function (res) {
-                    self.setOmit(res.data);
-                    resolve.call(self,res)
-                },
-                error:function (err) {
-                    reject.call(err);
-                }
-            })
-        });
-    }
-    getOpenCode(issue){
-        let self = this;
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url:'/get/openCode',
-                data:{
-                    issue:issue
-                },
-                dataType:'json',
-                success:function (res) {
-                    self.setOpenCode(res.data);
-                    resolve.call(self,res);
-                },
-                error:function (err) {
-                    reject.call(err)
-                }
-            })
-        })
+class Caculate {
+    computeCount(active,play_name){
+        let count = 0;
+        const exist = this.play_list.has(play_name)
+        const arr = new Array(active).fill('0');
+        if (exist&&play_name.at(0) === 'r'){
+            count = Caculate.combine(arr,play_name.split('')[1]);
+        }
+        return count;
     }
 
-    getState(issue){
-        let self = this;
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url:'/get/state',
-                data:{
-                    issue:issue
-                },
-                dataType:'json',
-                success:function (res) {
-                    resolve.call(self,res);
-                },
-                error:function (err) {
-                    reject.call(err)
+    static combine(arr,size){
+        let allResult = [];
+        (function f(arr,size,result) {
+            let arrLen = arr.length;
+            if (size>arrLen){
+                return;
+            }
+            if (size === arrLen){
+                allResult.push([].concat(result,arr))
+            } else {
+                for (let i=0;i<arrLen;i++){
+                    let newResult = [].concat(result);
+                    newResult.push(arr[i]);
+                    if (size === 1){
+                        allResult.push(newResult);
+                    } else {
+                        let newArr = [].concat(arr);
+                        newArr.split(0,i+1);
+                        f(newArr,size-1,newResult)
+                    }
                 }
-            })
-        });
+            }
+        })(arr,size,[])
     }
 }
-
-export default Interface
