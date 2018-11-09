a866188059ecbd5f4bb075d10814fa682b71d477
young <2461927976@qq.com>
2018-11-09 12:59:24 +0800

    实现了音乐播放器导航栏

--- music-vue/src/App.vue
+++ music-vue/src/App.vue
@@ -1,26 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <m-header></m-header>
     <!--<router-view/>-->
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  components: {
+    MHeader
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "common/stylus/variable.styl"
-/*#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}*/
-  #app
-    color: $color-theme
 </style>
