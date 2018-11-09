18d188691ab386ecf48f4d842e53662c2216bcd2
young <2461927976@qq.com>
2018-11-09 13:27:46 +0800

    完成页面骨架构建

---
+++ music-vue/src/components/singer/singer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    歌手页面
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- music-vue/src/main.js
+++ music-vue/src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
@@ -13,6 +13,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   render: h => h(App)
 })
