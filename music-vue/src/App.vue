d777356e4e7be958914c03f8fd6e1f23ac8cd9dc
young <2461927976@qq.com>
2018-11-09 10:47:02 +0800

    修改了相关es-lint配置，增加了stylus和stylus-loader模块

--- music-vue/src/App.vue
+++ music-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <!--<router-view/>-->
   </div>
 </template>
 
@@ -11,13 +11,16 @@ export default {
 }
 </script>
 
-<style>
-#app {
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/variable.styl"
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
+  #app
+    color: $color-theme
 </style>
