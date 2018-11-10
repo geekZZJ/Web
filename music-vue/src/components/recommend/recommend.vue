fe4fa638d2beaaea1447ce72344b31889c37197a
young <2461927976@qq.com>
2018-11-10 23:43:45 +0800

    实现了页面轮播效果

--- music-vue/src/components/recommend/recommend.vue
+++ music-vue/src/components/recommend/recommend.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper"></div>
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends" :key="item.value">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl"/>
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul></ul>
@@ -11,10 +19,17 @@
 </template>
 
 <script>
+import Slider from 'base/slider/slider'
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
+
+  data() {
+    return {
+      recommends: []
+    }
+  },
   created() {
     this._getRecommend()
   },
@@ -22,10 +37,13 @@ export default {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.slider)
+          this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
