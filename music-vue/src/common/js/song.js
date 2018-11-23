fa58bc0cf8941e488dc15b3ecb021dd9f887e47c
young <2461927976@qq.com>
2018-11-23 22:48:32 +0800

    成功获取qq音乐歌曲url接口

--- music-vue/src/common/js/song.js
+++ music-vue/src/common/js/song.js
@@ -20,7 +20,7 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=8490961695&vkey=DAEE311F06760D8537AF46DC5411E7847FDCB6E03ECBBB8B185D68ACCB1FF67C1B0C2BA3BAA56247BF27B0AF70ADB3AE657DF76CFC64AD65&uin=2600&fromtag=66`
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.strMediaMid}.m4a?guid=8490961695&vkey=DAEE311F06760D8537AF46DC5411E7847FDCB6E03ECBBB8B185D68ACCB1FF67C1B0C2BA3BAA56247BF27B0AF70ADB3AE657DF76CFC64AD65&uin=2600&fromtag=66`
   })
 }
 
