0292ea986d89729e6356790696678112ed96e609
young <2461927976@qq.com>
2020-01-27 19:01:57 +0800

    vue init初始化webpack

---
+++ vuejs/review/vue_demo/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'zzj&&lhy'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  color: red;
+}
+h1, h2 {
+  font-weight: normal;
+}
+</style>
