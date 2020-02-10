ab3d409008c99f55b278559a26f6777d80abda1f
young <2461927976@qq.com>
2020-02-10 21:21:10 +0800

    vuex

--- vuejs/review/vuex_test/src/App.vue
+++ vuejs/review/vuex_test/src/App.vue
@@ -9,45 +9,39 @@
 </template>
 
 <script>
-
+import {mapState, mapActions, mapGetters} from 'vuex'
 export default {
   name: 'App',
-  components: {
-  },
-  data () {
-    return {
-      count: 0
-    }
-  },
   computed: {
-    evenOrOdd () {
-      return this.count % 2 === 0 ? '偶数' : '奇数'
-    }
+    // evenOrOdd () {
+    //   return this.$store.getters.evenOrOdd
+    // }
+    count () {
+      return this.$store.getters.count
+    },
+    ...mapState(['count']), // mapState()返回值：{count(){return this.$store.state['count']}}
+    ...mapGetters(['evenOrOdd']) // mapGetters()返回值：{evenOrOdd(){return this.$store.getters['evenOrOdd']}}
   },
   methods: {
+    ...mapActions(['increment', 'decrement', 'incrementIfOdd', 'incrementAsync'])
+  }
+  /* methods: {
     increment () {
-      let count = this.count
-      this.count = count + 1
+      // 通知vuex去增加
+      this.$store.dispatch('increment') // 触发store中对应的action调用
     },
     decrement () {
-      let count = this.count
-      this.count = count - 1
+      this.$store.dispatch('decrement')
     },
     // 如果是奇数才增加
     incrementIfOdd () {
-      let count = this.count
-      if (count % 2 === 1) {
-        this.count = count + 1
-      }
+      this.$store.dispatch('incrementIfOdd')
     },
     // 过1s才增加
     incrementAsync () {
-      let count = this.count
-      setTimeout(() => {
-        this.count = count + 1
-      }, 1000)
+      this.$store.dispatch('incrementAsync')
     }
-  }
+  } */
 }
 </script>
 
