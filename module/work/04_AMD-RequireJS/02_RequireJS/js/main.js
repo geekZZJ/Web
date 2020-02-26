b676a43650f3e503cd5b1f3373f0264c03f6a456
young <2461927976@qq.com>
2020-02-26 22:00:48 +0800

    AMD规范

--- module/work/04_AMD-RequireJS/02_RequireJS/js/main.js
+++ module/work/04_AMD-RequireJS/02_RequireJS/js/main.js
@@ -3,12 +3,20 @@
         baseUrl: 'js/', //根目录 出发点在js目录下去找dataService和alerter
         paths: {
             dataService: 'modules/dataService',
-            alerter: 'modules/alerter'
+            alerter: 'modules/alerter',
+            jquery: 'libs/jquery-1.10.1',
+            angular: 'libs/angular'
+        },
+        shim: {
+            angular: {
+                exports: 'angular'
+            }
         }
     });
 
 
-    requirejs(['alerter'],function (alerter) {
+    requirejs(['alerter','angular'],function (alerter,angular) {
         alerter.showMsg()
+        console.log(angular)
     })
 })()
--- module/work/04_AMD-RequireJS/02_RequireJS/js/modules/alerter.js
+++ module/work/04_AMD-RequireJS/02_RequireJS/js/modules/alerter.js
@@ -1,10 +1,10 @@
 //定义有依赖的模块
-define(['dataService'],function (dataService) {
+define(['dataService','jquery'],function (dataService,$) {
     let msg = 'alerter.js'
     function showMsg() {
         console.log(msg,dataService.getName())
     }
-
+    $('body').css('background','deeppink')
     //暴露模块
     return {showMsg}
 })
