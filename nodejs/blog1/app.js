94825c3d988f90fb53ff54472bdc39298cdeb26e
young <2461927976@qq.com>
2020-04-16 18:48:16 +0800

    配置和使用redis

--- nodejs/blog1/app.js
+++ nodejs/blog1/app.js
@@ -6,7 +6,7 @@ const handleUserRouter = require('./src/router/user')
 const getCookieExpires = () => {
     const d = new Date()
     d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
-    console.log(d.toGMTString())
+    console.log('d.toGMTString() is ', d.toGMTString())
     return d.toGMTString()
 }
 
