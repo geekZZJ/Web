63bece339fc97c8474d28114219dffaaf29832cc
young <2461927976@qq.com>
2020-04-17 16:47:39 +0800

    深拷贝实现

---
+++ interview/变量类型和计算/deepClone.js
@@ -0,0 +1,43 @@
+/*
+* 深拷贝
+* */
+
+const obj1 = {
+    age: 20,
+    name: 'xxx',
+    address: {
+        city: "beijing"
+    },
+    arr: ['a', 'b', 'c']
+}
+
+const obj2 = deepClone(obj1)
+
+obj2.address.city = 'shanghai'
+console.log(obj1.address.city)
+
+/**
+ * 深拷贝
+ * @param obj 要拷贝的对象
+ */
+function deepClone(obj = {}) {
+    if (typeof obj !== 'object' || obj == null) {
+        //obj是null，或者不是对象和数组，直接返回
+        return obj
+    }
+    //初始化返回结果
+    let result
+    if (obj instanceof Array) {
+        result = []
+    } else {
+        result = {}
+    }
+    for (let key in obj) {
+        //保证key不是原型的属性
+        if (obj.hasOwnProperty(key)) {
+            //递归调用
+            result[key] = deepClone(obj[key])
+        }
+    }
+    return result
+}
