da486fd002486ae1481c2ec6942d039c2725137b
young <2461927976@qq.com>
2020-04-18 20:50:50 +0800

    更新fuxi 代码

---
+++ interview/作用域和闭包/bind.js
@@ -0,0 +1,25 @@
+//模拟bind
+Function.prototype.bind1 = function () {
+    //将参数拆解为数组
+    const args = Array.prototype.slice.call(arguments)
+    //获取this(数组第一项)
+    const t = args.shift()
+
+    //fn1.bind(...)中的fn1
+    const self = this
+
+    //返回一个函数
+    return function () {
+        return self.apply(t, args)
+    }
+}
+
+function fn1(a, b) {
+    console.log('this', this)
+    console.log(a, b)
+    return 'this is fn1'
+}
+
+const fn2 = fn1.bind1({x: 100}, 10, 20)
+const result = fn2()
+console.log(result)
