339243a3e3bd9f2965d4b96947072f74d7a82e48
young <2461927976@qq.com>
2020-04-18 20:52:06 +0800

    前后端联调

--- nodejs/blog1/src/router/user.js
+++ nodejs/blog1/src/router/user.js
@@ -1,4 +1,4 @@
-const {loginCheck} = require('../controller/user')
+const {login} = require('../controller/user')
 const {SuccessModel, ErrorModel} = require('../model/resModel')
 
 //获取cookie过期时间
@@ -13,10 +13,11 @@ const handleUserRouter = (req, res) => {
     const method = req.method
 
     //登录
-    if (method === 'GET' && req.path === '/api/user/login') {
-        // const {username, password} = req.body
-        const {username, password} = req.query
-        const result = loginCheck(username, password)
+    if (method === 'POST' && req.path === '/api/user/login') {
+        console.log(123)
+        const {username, password} = req.body
+        // const {username, password} = req.query
+        const result = login(username, password)
         return result.then(data => {
             if (data.username) {
                 //设置session
@@ -30,14 +31,14 @@ const handleUserRouter = (req, res) => {
     }
 
     //登录验证的测试
-    if (method === 'GET' && req.path === '/api/user/login-test') {
+    /*if (method === 'GET' && req.path === '/api/user/login-test') {
         if (req.session.username) {
             return Promise.resolve(new SuccessModel({
                 session: req.session
             }))
         }
         return Promise.resolve(new ErrorModel('尚未登录'))
-    }
+    }*/
 }
 
 module.exports = handleUserRouter
