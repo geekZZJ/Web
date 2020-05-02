8cbc4c8b0e056c83a0935a3b6cb9d0f7425fc0d3
young <2461927976@qq.com>
2020-05-02 11:21:27 +0800

    koa实现日志

--- nodejs/blog-koa2/app.js
+++ nodejs/blog-koa2/app.js
@@ -7,6 +7,9 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('koa-morgan')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -41,6 +44,19 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+    app.use(morgan('dev'))
+} else {
+    const logFileName = path.join(__dirname, 'logs', 'access.log')
+    const writeStream = fs.createWriteStream(logFileName, {
+        flags: 'a'
+    })
+    app.use(morgan('combined', {
+        stream: writeStream
+    }))
+}
 
 //session配置
 app.keys = ['Wjiol#2346_']
