256fe4d9e9a27b7b5b3d2a18826c15ed67c7b7a6
young <2461927976@qq.com>
2020-05-07 15:12:43 +0800

    学习线性回归

---
+++ AI/linear-regression/script.js
@@ -0,0 +1,20 @@
+import * as tf from '@tensorflow/tfjs'
+import * as tfvis from '@tensorflow/tfjs-vis'
+
+window.onload = () => {
+  const xs = [1, 2, 3, 4]
+  const ys = [1, 3, 5, 7]
+
+  tfvis.render.scatterplot(
+    { name: '线性回归训练集' },
+    {
+      values: xs.map((x, i) => {
+        return { x, y: ys[i] }
+      }),
+    },
+    { xAxisDomain: [0, 5], yAxisDomain: [0, 8] }
+  )
+
+  const model = tf.sequential()
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+}
