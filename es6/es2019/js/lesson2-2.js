470fd4c39308ff974cdf83b425108fbd5ed3dbbf
young <2461927976@qq.com>
2020-05-25 23:16:09 +0800

    array.of/fill

---
+++ es6/es2019/js/lesson2-2.js
@@ -0,0 +1,19 @@
+/**
+ * @name: lesson-
+ * @author: 张中俊
+ * @date: 2020/5/25 22:52
+ * @description：lesson-
+ * @update: 2020/5/25 22:52
+ */
+// Array.of()
+/*let arr = Array.of(1,2,3,4,5,6)
+console.log(arr)*/
+
+// Array.prototype.fill()
+/*
+let arr = new Array(7).fill(1)
+console.log(arr)*/
+
+// Array.prototype.fill(value,start,end)
+let arr = [1, 2, 3, 4, 5, 6]
+console.log(arr.fill(8, 2, 4))
