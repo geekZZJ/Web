c7a1bce5bc0ee70b0cd1d68d86d46fb0f45500d9
young <2461927976@qq.com>
2020-06-02 23:34:19 +0800

    RAF

---
+++ interview/question/RAF.js
@@ -0,0 +1,32 @@
+/**
+ * @name: RAF
+ * @author: 张中俊
+ * @date: 2020/6/2 23:19
+ * @description：RAF
+ * @update: 2020/6/2 23:19
+ */
+//3s把宽度从100px变成640px，即宽度增加540px
+//60帧/s，3s180帧，每次变化3px
+
+let curWidth = 100
+const maxWidth = 640
+const div1 = document.getElementById("div1")
+
+/*function animate() {
+  curWidth += 3
+  div1.style.width = curWidth + "px"
+  if (curWidth < maxWidth) {
+    setTimeout(animate, 16.7)  //自己控制时间
+  }
+}*/
+
+//RAF
+function animate() {
+  curWidth += 3
+  div1.style.width = curWidth + "px"
+  if (curWidth < maxWidth) {
+    window.requestAnimationFrame(animate)  //时间不用自己控制
+  }
+}
+
+animate()
