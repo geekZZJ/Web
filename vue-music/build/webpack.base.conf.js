92548ad96fae20d453401e9edfb5714fcf8ce810
young <2461927976@qq.com>
2020-06-17 23:07:36 +0800

    音乐app

--- vue-music/build/webpack.base.conf.js
+++ vue-music/build/webpack.base.conf.js
@@ -34,7 +34,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      '@': resolve('src'),
+      'src': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'api': resolve('src/api'),
     }
   },
   module: {
