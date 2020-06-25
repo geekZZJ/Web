eefddb78a1c382e7e61fc0c7bf21f7d15971d5a1
young <2461927976@qq.com>
2020-06-25 23:15:48 +0800

    优化细节，增加注释和css

--- react/react_init/src/App.js
+++ react/react_init/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import './style.css';
 
 /*function App() {
   return (
@@ -8,9 +9,12 @@ import React, {Component, Fragment} from 'react';
 class App extends Component {
   constructor() {
     super();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+
     this.state = {
       inputValue: 'hello',
-      list: ['learn 1', 'learn 2']
+      list: []
     }
   }
 
@@ -24,28 +28,55 @@ class App extends Component {
 
   handleKeyUp(e) {
     if (e.keyCode === 13) {
+      if (this.state.inputValue === '') {
+        console.log("输入内容为空！")
+        return
+      }
       const list = [...this.state.list, this.state.inputValue];
       this.setState({
-        list
+        list,
+        inputValue: ''
       })
     }
   }
 
+  handleItemClick(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    // console.log(list);
+    this.setState({
+      list
+    })
+  }
+
+  getListItems() {
+    return this.state.list.map((value, index) => {
+      return (
+        <li key={index}
+            onClick={this.handleItemClick.bind(this, index)}
+            dangerouslySetInnerHTML={{__html: value}}
+        >
+        </li>
+      )
+    })
+  }
+
   render() {
+    // 这是普通的注释
     return (
       <Fragment>
+        {/*这是一个lable*/}
+        <label htmlFor='myinput'>请输入内容：</label>
         <input
+          id='myinput'
+          className='input'
           type="text"
           value={this.state.inputValue}
-          onChange={this.handleInputChange.bind(this)}
-          onKeyUp={this.handleKeyUp.bind(this)}
+          onChange={this.handleInputChange}
+          onKeyUp={this.handleKeyUp}
         />
         <ul>
-          {
-            this.state.list.map((value, index) => {
-              return <li key={index}>{value}</li>
-            })
-          }
+          {this.getListItems()}
         </ul>
       </Fragment>
     )
