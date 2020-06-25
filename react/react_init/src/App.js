f9305a34665b64e8d02dc35c8303e2bc0ef4b589
young <2461927976@qq.com>
2020-06-25 21:46:55 +0800

    开始todoList项目

--- react/react_init/src/App.js
+++ react/react_init/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <div>TodoList</div>
   );
 }
 
