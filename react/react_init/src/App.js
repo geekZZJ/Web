b6ba7a9784f3369e2ddad4fea4754fa26c31b9a4
young <2461927976@qq.com>
2020-06-25 22:20:07 +0800

    初始化

--- react/react_init/src/App.js
+++ react/react_init/src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 
-function App() {
+/*function App() {
   return (
     <div>TodoList</div>
   );
+}*/
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: 'hello',
+      list: ['learn 1', 'learn 2']
+    }
+  }
+
+  handleInputChange(e) {
+    // console.log(e.target.value);
+    // console.log(this);
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange.bind(this)}
+        />
+        <ul>
+          {
+            this.state.list.map((value, index) => {
+              return <li key={index}>{value}</li>
+            })
+          }
+        </ul>
+      </Fragment>
+    )
+  }
 }
 
 export default App;
