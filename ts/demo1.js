aa5751d1de5a143f1f934e2550643e258a0b81d2
young <2461927976@qq.com>
2020-06-26 21:44:15 +0800

    typescript

---
+++ ts/demo1.js
@@ -0,0 +1,5 @@
+function demo(data) {
+  return Math.sqrt(data.x ** 2 + data.y ** 2);
+}
+
+demo()
---
+++ ts/demo1.ts
@@ -0,0 +1,11 @@
+// type Point = { x: number, y: number };
+interface Point {
+  x: number,
+  y: number
+}
+
+function demo(data: Point) {
+  return Math.sqrt(data.x ** 2 + data.y ** 2);
+}
+
+demo({x: 1, y: 2});
