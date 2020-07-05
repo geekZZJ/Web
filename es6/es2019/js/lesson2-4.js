05db492e61d24a5d1d7b07c698901965794de9e6
young <2461927976@qq.com>
2020-07-05 21:56:43 +0800

    rest参数

--- es6/es2019/js/lesson2-4.js
+++ es6/es2019/js/lesson2-4.js
@@ -6,6 +6,29 @@
  * @update: 2020/5/26 11:58
  */
 // 函数默认值
-function f(x = 1, y = 2, z = 3) {
+/*
+function f (x = 1, y = 2, z = 3) {
   return x + y + z
+} */
+
+/* function sum () {
+  let num = 0
+  // es5
+  /!* Array.prototype.forEach.call(arguments, function (item) {
+    num += item * 1;
+  }) *!/
+  // es6
+  Array.from(arguments).forEach(function (item) {
+    num += item * 1
+  })
+  return num
+} */
+function sum (base, ...nums) {
+  let num = 0
+  nums.forEach(function (item) {
+    num += item * 1
+  })
+  return base * 2 + num
 }
+
+console.log(sum(1, 2, 3, 5))
