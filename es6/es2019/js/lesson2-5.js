8cb0bb10936ef1ae20cde6ebe23f1a4002a6ac04
young <2461927976@qq.com>
2020-07-06 21:47:47 +0800

    map数据类型

--- es6/es2019/js/lesson2-5.js
+++ es6/es2019/js/lesson2-5.js
@@ -1,5 +1,5 @@
 // Object
-let x = 1
+/* let x = 1
 let y = 2
 let z = 3
 let obj = {
@@ -11,4 +11,23 @@ let obj = {
   }
 }
 console.log(obj)
-obj.hello()
+obj.hello() */
+let map = new Map()
+map.set(1, 2)
+map.set(3, 5)
+map.set(1, 3)
+// map.delete(1)
+// map.clear()
+/* console.log(map.size)
+console.log(map.has(2)) */
+
+/* console.log(map.get(1))
+console.log(map.keys(), map.values()) */
+
+/* map.forEach((value, key) => {
+  console.log(value, key)
+}) */
+
+for (let [key, value] of map) {
+  console.log(key, value)
+}
