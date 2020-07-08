020cdf6a836dba644ffd1184b4175e2b7bcee4fa
young <2461927976@qq.com>
2020-07-08 17:08:57 +0800

    promise.all

--- es6/es2019/js/lesson2-9.js
+++ es6/es2019/js/lesson2-9.js
@@ -13,7 +13,7 @@ function test () {
 
 loadScript('./1.js', test) */
 
-function loadScript (src) {
+/* function loadScript (src) {
   return new Promise((resolve, reject) => {
     let script = document.createElement('script')
     script.src = src
@@ -25,8 +25,15 @@ function loadScript (src) {
     }
     document.head.append(script)
   })
-}
+} */
 
-loadScript('./1.js')
+/* loadScript('./1.js')
   .then(loadScript('./2.js'))
-  .then(loadScript('./3.js'))
+  .then(loadScript('./3.js')) */
+const p1 = Promise.resolve(1)
+const p2 = Promise.resolve(2)
+const p3 = Promise.resolve(3)
+
+Promise.all([p1, p2, p3]).then(value => {
+  console.log(value)
+})
