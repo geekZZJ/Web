f4f924c97cb7845afc59ba93ff9025a7853fff5a
young <2461927976@qq.com>
2020-07-26 18:31:38 +0800

    yield无返回值

--- es6/es2019/js/lesson2-12.js
+++ es6/es2019/js/lesson2-12.js
@@ -1,4 +1,4 @@
-function * loop () {
+/* function * loop () {
   for (let i = 0; i < 5; i++) {
     yield console.log(i)
   }
@@ -6,5 +6,15 @@ function * loop () {
 
 const l = loop()
 l.next()
+l.next()
+l.next() */
+
+function * gen () {
+  let val
+  val = yield 1
+  console.log(val)
+}
+
+const l = gen()
 l.next()
 l.next()
