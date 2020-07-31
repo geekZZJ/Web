301205996c892bb9544ba0c29058a34efde25331
young <2461927976@qq.com>
2020-07-31 14:24:17 +0800

    es10内容

--- es6/es2019/js/lesson6-1.js
+++ es6/es2019/js/lesson6-1.js
@@ -1,3 +1,27 @@
-console.log(JSON.stringify('\u{D800}'))
+/* console.log(JSON.stringify('\u{D800}'))
 let arr = [1, [2, 3], [4, 5, [6, 7]]]
-console.log(arr.flat(2))
+console.log(arr.flat(2)) */
+
+let str = `"foo" and "bar" and "baz"`
+
+/* function select (regExp, str) {
+  const matches = []
+  while (true) {
+    const match = regExp.exec(str)
+    if (match === null) break
+    matches.push(match)
+  }
+  return matches
+}
+
+console.log(select(/"([^"]*)"/g, str)) */
+
+function select (regExp, str) {
+  const matches = []
+  for (const match of str.matchAll(regExp)) {
+    matches.push(match[1])
+  }
+  return matches
+}
+
+console.log(select(/"([^"]*)"/g, str))
