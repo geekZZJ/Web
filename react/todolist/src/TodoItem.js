967f0ae5beda56062a58c792e46fba7108fefac7
young <2461927976@qq.com>
2020-08-02 14:52:59 +0800

    todolist优化

---
+++ react/todolist/src/TodoItem.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+
+class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  render() {
+    const {content} = this.props
+    return (
+      <li
+        onClick={this.handleClick}
+      >
+        {content}
+      </li>
+    )
+  }
+
+  handleClick() {
+    const {deleteItem, index} = this.props
+    deleteItem(index)
+  }
+}
+
+export default TodoItem
--- react/todolist/src/TodoList.js
+++ react/todolist/src/TodoList.js
@@ -1,4 +1,6 @@
 import React, {Component, Fragment} from 'react';
+import './style.css'
+import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
   constructor(props) {
@@ -7,6 +9,9 @@ class TodoList extends Component {
       inputValue: '',
       list: []
     }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleItemDelete = this.handleItemDelete.bind(this)
   }
 
   render() {
@@ -14,16 +19,17 @@ class TodoList extends Component {
       <Fragment>
         <div>
           {/*下面是一个input框*/}
+          <label htmlFor="insert">输入内容</label>
           <input type="text"
+                 id="insert"
+                 className="input"
                  value={this.state.inputValue}
-                 onChange={this.handleInputChange.bind(this)}
+                 onChange={this.handleInputChange}
           />
-          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+          <button onClick={this.handleBtnClick}>提交</button>
         </div>
         <ul>
-          {this.state.list.map((item, index) => {
-            return <li key={index} onClick={this.handleItemDelete.bind(this, index)}>{item}</li>
-          })}
+          {this.getTodoItem()}
         </ul>
       </Fragment>
     )
@@ -31,16 +37,41 @@ class TodoList extends Component {
 
   handleInputChange(e) {
     // console.log(e.target.value)
-    this.setState({
+    /*this.setState({
       inputValue: e.target.value
+    })*/
+    const value = e.target.value
+    this.setState(() => {
+      return {
+        inputValue: value
+      }
+    })
+  }
+
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={item}
+          index={index}
+          deleteItem={this.handleItemDelete}
+        />
+      )
     })
   }
 
   handleBtnClick() {
     if (this.state.inputValue !== "") {
-      this.setState({
+      /*this.setState({
         list: [...this.state.list, this.state.inputValue],
         inputValue: ""
+      })*/
+      this.setState((prevState) => {
+        return {
+          list: [...prevState.list, prevState.inputValue],
+          inputValue: ""
+        }
       })
     } else {
       alert("内容不能为空！")
