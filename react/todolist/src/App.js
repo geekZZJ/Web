1fa59f707d4c642cf39e26409128d12a457fc30f
young <2461927976@qq.com>
2020-08-04 10:57:12 +0800

    react css实现过渡动画

--- react/todolist/src/App.js
+++ react/todolist/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
+import './app.css'
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    }
+    this.handleToggle = this.handleToggle.bind(this)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+        <button onClick={this.handleToggle}>toggle</button>
+      </Fragment>
+    )
+  }
+
+  handleToggle() {
+    this.setState({
+      show: this.state.show ? false : true
+    })
+  }
 }
 
 export default App;
