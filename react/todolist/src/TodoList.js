59b8f736cee6e91ff7a190645ad98437784b131c
young <2461927976@qq.com>
2020-08-07 11:02:56 +0800

    action和reducer编写

--- react/todolist/src/TodoList.js
+++ react/todolist/src/TodoList.js
@@ -1,33 +1,58 @@
 import React, {Component, Fragment} from 'react';
 import 'antd/dist/antd.css';
 import {Input, Button, List} from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import store from "./store";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState()
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    store.subscribe(this.handleStoreChange)
+  }
+
   render() {
     return (
       <Fragment>
         <Fragment>
-          <Input placeholder="Todo Info" style={{width: '300px', marginRight: '10px'}}/>
-          <Button type="primary">提交</Button>
+          <Input value={this.state.inputValue}
+                 placeholder="Todo Info"
+                 style={{width: '300px', marginRight: '10px'}}
+                 onChange={this.handleInputChange}
+          />
+          <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
         </Fragment>
         <List
           style={{marginTop: "10px", width: "380px"}}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (
             <List.Item>{item}</List.Item>
           )}
         />
       </Fragment>
     )
+  }
+
+  handleInputChange(e) {
+    const action = {
+      type: "change_input_value",
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
+
+  handleBtnClick() {
+    const action = {
+      type: "add_todo_item"
+    }
+    store.dispatch(action)
   }
 }
 
---
+++ react/todolist/src/store/reducer.js
@@ -0,0 +1,21 @@
+const defaultState = {
+  inputValue: '',
+  list: [1, 2]
+}
+
+// reducer可以接收state,但是不能修改state
+export default (state = defaultState, action) => {
+  if (action.type === 'change_input_value') {
+    // 深拷贝
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value
+    return newState
+  }
+  if (action.type === "add_todo_item") {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ""
+    return newState
+  }
+  return state
+}
