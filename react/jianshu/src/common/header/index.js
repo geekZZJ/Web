8181ef0c559ee9f3c11fd3bc4a2c0ce599abeea4
young <2461927976@qq.com>
2020-08-11 11:54:07 +0800

    style component写header

--- react/jianshu/src/common/header/index.js
+++ react/jianshu/src/common/header/index.js
@@ -1,11 +1,22 @@
 import React, {Component} from "react";
-import {HeaderWrapper, Logo} from "./style"
+import {Addtion, Button, HeaderWrapper, Logo, Nav, NavItem, NavSearch} from "./style"
 
 class Header extends Component {
   render() {
     return (
       <HeaderWrapper>
         <Logo/>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载APP</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavSearch/>
+          <Addtion>
+            <Button className="writting">写文章</Button>
+            <Button className="reg">注册</Button>
+          </Addtion>
+        </Nav>
       </HeaderWrapper>
     );
   }
