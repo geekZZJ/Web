21f2e6553f204f414f0652a484953acf0f3b4a0c
zzj <2461927976@qq.com>
2020-08-22 17:26:51 +0800

    react路由

--- react/jianshu/src/App.js
+++ react/jianshu/src/App.js
@@ -2,12 +2,17 @@ import React, {Component} from 'react';
 import {Provider} from "react-redux"
 import Header from "./common/header";
 import store from "./store";
+import {BrowserRouter, Route} from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Header/>
+        <BrowserRouter>
+          <Route path="/" exact render={() => <div>首页</div>}></Route>
+          <Route path="/detail" exact render={() => <div>detail</div>}></Route>
+        </BrowserRouter>
       </Provider>
     )
   }
