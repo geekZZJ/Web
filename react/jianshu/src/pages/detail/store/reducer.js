e8da7ae878ee297f4eeab8f8aaa528dd0b07b4c8
zzj <2461927976@qq.com>
2020-08-27 19:46:41 +0800

    使用redux管理详情页面数据

---
+++ react/jianshu/src/pages/detail/store/reducer.js
@@ -0,0 +1,15 @@
+import {fromJS} from "immutable"
+import * as contants from "./constants";
+
+const defaultState = fromJS({
+  title: "杨丽萍徒弟水月与同性结婚，在中国允许同性恋了吗？",
+  content: '<img src="//upload-images.jianshu.io/upload_images/7190121-750d12d141de3e5c.JPEG?imageMogr2/auto-orient/strip|imageView2/2/w/640/format/webp" alt=""/><p>杨丽萍徒弟水月和同性女人结婚，上了热搜，也抢了我的眼球。在中国，同性恋可以领证吗？合法吗？啥时可以同性恋结婚了？</p><p>在中国，同性恋是不可以领证的。但是不违法！所以，水月和同性女人结婚，也算是打着擦边球，而且身不由己的赚足了国人的眼球。</p><p>其实，同性恋不被支持点赞，由来已久。但是，似乎也没有阻挡住，爱情的脚步。两个同性的人，就是恋了，爱了，打算一辈子在一起，也没有妨碍多少人的事吧？人家就是为了自己而活，怎么了？</p>'
+})
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
