10202651fa580df5c0ca45039588b746c83692fb
zzj <2461927976@qq.com>
2020-08-27 16:05:39 +0800

    返回顶部功能实现

--- react/jianshu/src/pages/home/index.js
+++ react/jianshu/src/pages/home/index.js
@@ -3,11 +3,19 @@ import Topic from "./components/Topic";
 import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
-import {HomeLeft, HomeRight, HomeWrapper} from "./style";
+import {BackTop, HomeLeft, HomeRight, HomeWrapper} from "./style";
 import {connect} from "react-redux";
 import {actionCreator} from "./store"
 
 class Home extends Component {
+  handleScrollTop() {
+    window.scrollTo(0, 0)
+  }
+
+  bindEvents() {
+    window.addEventListener("scroll", this.props.changeScrollTopShow)
+  }
+
   render() {
     return (
       <HomeWrapper>
@@ -22,20 +30,37 @@ class Home extends Component {
           <Recommend/>
           <Writer/>
         </HomeRight>
+        {this.props.showScroll ? <BackTop onClick={this.handleScrollTop}>顶部</BackTop> : null}
       </HomeWrapper>
     );
   }
 
   componentDidMount() {
     this.props.changeHomeData()
+    this.bindEvents()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.props.changeScrollTopShow)
   }
 }
+
+const mapState = state => ({
+  showScroll: state.getIn(["home", "showScroll"])
+})
 
 const mapDispatch = dispatch => ({
   changeHomeData() {
     const action = actionCreator.getHomeInfo()
     dispatch(action)
+  },
+  changeScrollTopShow() {
+    if (document.documentElement.scrollTop > 100) {
+      dispatch(actionCreator.toggleTopShow(true))
+    } else {
+      dispatch(actionCreator.toggleTopShow(false))
+    }
   }
 })
 
-export default connect(null, mapDispatch)(Home)
+export default connect(mapState, mapDispatch)(Home)
--- react/jianshu/src/pages/home/store/reducer.js
+++ react/jianshu/src/pages/home/store/reducer.js
@@ -5,22 +5,33 @@ const defaultState = fromJS({
   topicList: [],
   articleList: [],
   recommendList: [],
-  articlePage: 1
+  articlePage: 1,
+  showScroll: false
 })
+
+const chageHomeData = (state, action) => {
+  return state.merge({
+    topicList: fromJS(action.topicList),
+    articleList: fromJS(action.articleList),
+    recommendList: fromJS(action.recommendList)
+  })
+}
+
+const addArticleList = (state, action) => {
+  return state.merge({
+    articleList: state.get("articleList").concat(action.list),
+    articlePage: action.nextPage
+  })
+}
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case contants.CHANGE_HOME_DATA:
-      return state.merge({
-        topicList: fromJS(action.topicList),
-        articleList: fromJS(action.articleList),
-        recommendList: fromJS(action.recommendList)
-      })
+      return chageHomeData(state, action)
     case contants.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get("articleList").concat(action.list),
-        articlePage: action.nextPage
-      })
+      return addArticleList(state, action)
+    case contants.TOGGLE_SCROLL_TOP:
+      return state.set("showScroll", action.show)
     default:
       return state
   }
