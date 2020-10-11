7abc6732b01c46ff6e68f051f0290be302626c50
zzj <2461927976@qq.com>
2020-10-11 20:33:53 +0800

    koa常用

--- koa2/first/index.js
+++ koa2/first/index.js
@@ -2,26 +2,62 @@
  * @Author: 张中俊
  * @Date: 2020-10-11 17:02:03
  * @LastEditors: 张中俊
- * @LastEditTime: 2020-10-11 17:31:28
+ * @LastEditTime: 2020-10-11 20:31:13
  * @Description:
  */
 const Koa = require("koa");
 const Router = require("koa-router");
+const cors = require("@koa/cors");
+const koaBody = require("koa-body");
+const json = require("koa-json");
 const app = new Koa();
 const router = new Router();
 
+router.prefix("/api");
+
 router.get("/", (ctx) => {
-  console.log(ctx);
+  // console.log(ctx);
   console.log(ctx.request);
   ctx.body = "hello world";
 });
 
 router.get("/api", (ctx) => {
-  console.log(ctx);
+  const params = ctx.request.query;
+  console.log(params);
+  // console.log(ctx);
   console.log(ctx.request);
-  ctx.body = "hello API";
+  ctx.body = {
+    name: params.name,
+    age: params.age,
+  };
 });
 
+router.get("/async", async (ctx) => {
+  const result = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("hello world delay 2s");
+    }, 2000);
+  });
+  ctx.body = result;
+});
+
+router.post("/post", async (ctx) => {
+  let { body } = ctx.request;
+  console.log(body);
+  console.log(ctx.request);
+  ctx.body = {
+    ...body,
+  };
+});
+
+app.use(koaBody());
+app.use(cors());
+app.use(
+  json({
+    pretty: false,
+    param: "pretty",
+  })
+);
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
