c9f85cb7a58d84ad968bfbc9111f4df16e0eb4df
zzj <2461927976@qq.com>
2020-10-13 21:40:37 +0800

    koa热加载，es6语法支持

--- koa2/first/src/index.js
+++ koa2/first/src/index.js
@@ -2,16 +2,15 @@
  * @Author: 张中俊
  * @Date: 2020-10-12 20:10:59
  * @LastEditors: 张中俊
- * @LastEditTime: 2020-10-12 20:38:00
+ * @LastEditTime: 2020-10-12 23:01:30
  * @Description:
  */
-const koa = require("koa");
+import koa from "koa";
+import path from "path";
+import helmet from "koa-helmet";
+import statics from "koa-static";
+import router from "./routes/routes";
 const app = new koa();
-const path = require("path");
-const helmet = require("koa-helmet");
-const statics = require("koa-static");
-
-const router = require("./routes/routes");
 
 app.use(helmet());
 app.use(statics(path.join(__dirname, "/public")));
---
+++ koa2/first/webpack.config.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: 张中俊
+ * @Date: 2020-10-12 20:55:05
+ * @LastEditors: 张中俊
+ * @LastEditTime: 2020-10-12 22:58:11
+ * @Description:
+ */
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const webpackconfig = {
+  target: "node",
+  mode: "development",
+  entry: {
+    server: path.join(__dirname, "src/index.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "./dist"),
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: [path.join(__dirname, "/node_modules")],
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+  plugins: [new CleanWebpackPlugin()],
+  node: {
+    global: true,
+    __filename: true,
+    __dirname: true,
+  },
+};
+
+module.exports = webpackconfig;
