b1991ea085a3f5986a027278e041d33ab835b738
zzj <2461927976@qq.com>
2020-10-31 17:44:03 +0800

    tensorflow在浏览器安装

---
+++ AI/setup/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: zzj
+ * @Date: 2020-10-31 17:25:16
+ * @LastEditors: zzj
+ * @LastEditTime: 2020-10-31 17:41:23
+ * @Description:
+ */
+import * as tf from "@tensorflow/tfjs";
+
+const a = tf.tensor([1, 2]);
+a.print();
