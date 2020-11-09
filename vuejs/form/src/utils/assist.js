35c4581055ac70faf91e931cf04481ce6bfe1a2c
zzj <2461927976@qq.com>
2020-11-09 16:28:50 +0800

    向上向下找到最近和全部的组件实例

---
+++ vuejs/form/src/utils/assist.js
@@ -0,0 +1,64 @@
+/*
+ * @Author: zzj
+ * @Date: 2020-11-06 16:48:59
+ * @LastEditors: zzj
+ * @LastEditTime: 2020-11-09 16:27:21
+ * @Description:
+ */
+// 由一个组件，向上找到最近的指定组件
+function findComponentUpward(context, componentName) {
+  let parent = context.$parent;
+  let name = parent.$options.name;
+  while (parent && (!name || [componentName].indexOf(name) < 0)) {
+    parent = parent.$parent;
+    if (parent) name = parent.$options.name;
+  }
+  return parent;
+}
+
+// 由一个组件，向上找到所有的指定组件
+function findComponentsUpward(context, componentName) {
+  let parents = [];
+  const parent = context.$parent;
+  if (parent) {
+    if (parent.$options.name === componentName) parents.push(parent);
+    return parents.concat(findComponentsUpward(parent, componentName));
+  } else {
+    return [];
+  }
+}
+
+// 由一个组件，向下找到最近的指定组件
+function findComponentDownward(context, componentName) {
+  const childrens = context.$children;
+  let children = null;
+  if (childrens.length) {
+    for (const child of childrens) {
+      const name = child.$options.name;
+      if (name === componentName) {
+        children = child;
+        break;
+      } else {
+        children = findComponentDownward(child, componentName);
+        if (children) break;
+      }
+    }
+  }
+  return children;
+}
+
+// 由一个组件，向下找到所有指定的组件
+function findComponentsDownward(context, componentName) {
+  return context.$children.reduce((components, child) => {
+    if (child.$options.name === componentName) components.push(child);
+    const foundChilds = findComponentsDownward(child, componentName);
+    return components.concat(foundChilds);
+  }, []);
+}
+
+export {
+  findComponentUpward,
+  findComponentsUpward,
+  findComponentDownward,
+  findComponentsDownward,
+};
