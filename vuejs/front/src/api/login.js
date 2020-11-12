8ff2332ec2bfbf92b91e319488bd3b58c7557d3b
zzj <2461927976@qq.com>
2020-11-12 10:59:59 +0800

    修改接口地址

--- vuejs/front/src/api/login.js
+++ vuejs/front/src/api/login.js
@@ -2,21 +2,21 @@
  * @Author: zzj
  * @Date: 2020-10-18 21:37:59
  * @LastEditors: zzj
- * @LastEditTime: 2020-11-08 14:45:56
+ * @LastEditTime: 2020-11-11 16:50:47
  * @Description:
  */
 import axios from "@/utils/request";
 
 const getCode = (sid) => {
-  return axios.get("/getCaptcha", { params: { sid } });
+  return axios.get("/public/getCaptcha", { params: { sid } });
 };
 
 const forget = (option) => {
-  return axios.post("/forget", { ...option });
+  return axios.post("/login/forget", { ...option });
 };
 
 const login = (loginInfo) => {
-  axios.post("/login", { ...loginInfo });
+  axios.post("/login/login", { ...loginInfo });
 };
 
 export { getCode, forget, login };
