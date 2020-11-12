7c0c1635af89dacd893305474ce6edb691efa8ec
zzj <2461927976@qq.com>
2020-11-12 11:03:08 +0800

    配置文件修改

--- koa2/first/src/config/index.js
+++ koa2/first/src/config/index.js
@@ -2,13 +2,13 @@
  * @Author: zzj
  * @Date: 2020-11-04 15:17:41
  * @LastEditors: zzj
- * @LastEditTime: 2020-11-11 10:47:34
+ * @LastEditTime: 2020-11-12 11:02:53
  * @Description:
  */
-const DB_URL = "mongodb://test:123456@114.55.102.168:27017/test";
+const DB_URL = "mongodb://test:123456@ip:port/test";
 const REDIS = {
-  host: "114.55.102.168",
-  port: "15001",
+  host: "ip",
+  port: "port",
   password: "123456",
 };
 const JWT_SECRET = "123456";
