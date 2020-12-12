a66eaf308f5fab28cf8b89a01698c5b161aee891
zzj <2461927976@qq.com>
2020-12-12 15:12:32 +0800

    使用cheerio进行数据提取

--- ts/03/src/crawler.ts
+++ ts/03/src/crawler.ts
@@ -2,17 +2,38 @@
  * @Author: zzj
  * @Date: 2020-12-11 22:04:34
  * @LastEditors: zzj
- * @LastEditTime: 2020-12-12 12:24:14
+ * @LastEditTime: 2020-12-12 15:12:00
  * @Description:
  */
 import superagent from "superagent";
+import cheerio from "cheerio";
+
+interface Blog {
+  title: string;
+  count: number;
+}
 
 class Crawler {
-  private url = "https://www.csdn.net/nav/web?spm=1000.2115.3001.4125";
-  private rawHtml = "";
+  private url = "https://www.jianshu.com/";
+
+  getBlogInfo(html: string) {
+    const $ = cheerio.load(html);
+    const blogItems = $(".content");
+    const blogInfos: Blog[] = [];
+    blogItems.map((index, element) => {
+      const title = $(element).find(".title").text().trim();
+      const count = parseInt($(element).find(".meta span").eq(1).text());
+      blogInfos.push({ title, count });
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: blogInfos,
+    };
+  }
+
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.rawHtml = result.text;
+    this.getBlogInfo(result.text);
   }
   constructor() {
     this.getRawHtml();
