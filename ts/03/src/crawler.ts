85a6fc9448d87967536353b42b15982438dd8e65
zzj <2461927976@qq.com>
2020-12-12 16:25:38 +0800

    爬取数据的结构设计和存储

--- ts/03/src/crawler.ts
+++ ts/03/src/crawler.ts
@@ -2,15 +2,26 @@
  * @Author: zzj
  * @Date: 2020-12-11 22:04:34
  * @LastEditors: zzj
- * @LastEditTime: 2020-12-12 15:12:00
+ * @LastEditTime: 2020-12-12 16:24:18
  * @Description:
  */
+import fs from "fs";
+import path from "path";
 import superagent from "superagent";
 import cheerio from "cheerio";
 
 interface Blog {
   title: string;
   count: number;
+}
+
+interface BlogResult {
+  time: number;
+  data: Blog[];
+}
+
+interface Content {
+  [propName: number]: Blog[];
 }
 
 class Crawler {
@@ -25,7 +36,7 @@ class Crawler {
       const count = parseInt($(element).find(".meta span").eq(1).text());
       blogInfos.push({ title, count });
     });
-    const result = {
+    return {
       time: new Date().getTime(),
       data: blogInfos,
     };
@@ -33,10 +44,29 @@ class Crawler {
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.getBlogInfo(result.text);
+    return result.text;
   }
+
+  generateJsonContent(blogInfo: BlogResult) {
+    const filePath = path.resolve(__dirname, "../data/blog.json");
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    }
+    fileContent[blogInfo.time] = blogInfo.data;
+    return fileContent;
+  }
+
+  async initSpiderProcess() {
+    const filePath = path.resolve(__dirname, "../data/blog.json");
+    const html = await this.getRawHtml();
+    const blogInfo = await this.getBlogInfo(html);
+    const fileContent = this.generateJsonContent(blogInfo);
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+  }
+
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProcess();
   }
 }
 
