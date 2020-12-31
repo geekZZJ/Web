d75d7c55f9fb8533798d534f65a46d91ad777cc8
zzj <2461927976@qq.com>
2020-12-31 17:08:18 +0800

    使用parcel

---
+++ ts/parcel/src/page.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: zzj
+ * @Date: 2020-12-31 16:58:46
+ * @LastEditors: zzj
+ * @LastEditTime: 2020-12-31 17:01:02
+ * @Description:
+ */
+const teacher: string = "dell";
+console.log(teacher);
