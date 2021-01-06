074de594606ed2922234cbc4ff2a669cd237e971
zzj <2461927976@qq.com>
2021-01-06 14:38:30 +0800

    手写new

---
+++ interview/question/new.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: zzj
+ * @Date: 2021-01-06 14:00:55
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-01-06 14:34:25
+ * @Description:手写new
+ */
+function create() {
+  // 创建一个空对象
+  let obj = {};
+  // 获取构造函数
+  let Con = [].shift.call(arguments);
+  // 设置空对象的原型
+  obj.__proto__ = Con.prototype;
+  // 绑定 this 并执行构造函数
+  let result = Con.apply(obj, arguments);
+  // 确保返回值为对象
+  return result instanceof Object ? result : obj;
+}
