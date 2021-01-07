cb9101092ea3b2931283dd10e6271eedf8c5c6ae
zzj <2461927976@qq.com>
2021-01-07 11:30:03 +0800

    依赖收集追踪原理，第三节

---
+++ interview/MVVM/simple_vue/vue3.js
@@ -0,0 +1,89 @@
+/*
+ * @Author: zzj
+ * @Date: 2021-01-07 10:16:01
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-01-07 11:23:52
+ * @Description:
+ */
+class Dep {
+  constructor() {
+    /* 用来存放Watcher对象的数组 */
+    this.subs = [];
+  }
+
+  /* 在subs中添加一个Watcher对象 */
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+
+  /* 通知所有Watcher对象更新视图 */
+  notify() {
+    this.subs.forEach((sub) => {
+      sub.update();
+    });
+  }
+}
+
+class Watcher {
+  constructor() {
+    /* 在new一个Watcher对象时将该对象赋值给Dep.target，在get中会用到 */
+    Dep.target = this;
+  }
+
+  /* 更新视图的方法 */
+  update() {
+    console.log("视图更新啦～");
+  }
+}
+
+function observer(value) {
+  if (!value || typeof value !== "object") {
+    return;
+  }
+
+  Object.keys(value).forEach((key) => {
+    defineReactive(value, key, value[key]);
+  });
+}
+
+function defineReactive(obj, key, val) {
+  /* 一个Dep类对象 */
+  const dep = new Dep();
+
+  Object.defineProperty(obj, key, {
+    enumerable: true /* 属性可枚举 */,
+    configurable: true /* 属性可被修改或删除 */,
+    get: function reactiveGetter() {
+      /* 将Dep.target（即当前的Watcher对象存入dep的subs中） */
+      dep.addSub(Dep.target);
+      return val;
+    },
+    set: function reactiveSetter(newVal) {
+      if (newVal === val) return;
+      val = newVal;
+      /* 在set的时候触发dep的notify来通知所有的Watcher对象更新视图 */
+      dep.notify();
+    },
+  });
+}
+
+class Vue {
+  constructor(options) {
+    this._data = options.data;
+    observer(this._data);
+    /* 新建一个Watcher观察者对象，这时候Dep.target会指向这个Watcher对象 */
+    new Watcher();
+    /* 在这里模拟render的过程，为了触发test属性的get函数 */
+    console.log("render~", this._data.test);
+  }
+}
+
+let o = new Vue({
+  data: {
+    test: "I am test.",
+  },
+});
+
+o._data.test = "hello,world.";
+
+Dep.target = null;
