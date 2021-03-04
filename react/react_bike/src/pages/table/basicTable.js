c8b733ed7a5204a69fd04800bf9f684c70374506
zzj <2461927976@qq.com>
2021-03-04 10:52:24 +0800

    表格点击行复选框选中

--- react/react_bike/src/pages/table/basicTable.js
+++ react/react_bike/src/pages/table/basicTable.js
@@ -2,7 +2,7 @@
  * @Author: zzj
  * @Date: 2021-03-03 16:55:48
  * @LastEditors: zzj
- * @LastEditTime: 2021-03-03 21:04:13
+ * @LastEditTime: 2021-03-04 10:49:10
  * @Description:
  */
 import React from "react";
@@ -37,9 +37,10 @@ const columns = [
     dataIndex: "address",
   },
 ];
+
 const data = [
   {
-    id: "1",
+    id: 1,
     username: "John Brown",
     gender: "男",
     state: 0,
@@ -47,23 +48,61 @@ const data = [
     birthday: "19980226",
     address: "New York No. 1 Lake Park",
   },
+  {
+    id: 2,
+    username: "John Brown1",
+    gender: "女",
+    state: 1,
+    hobby: "test1",
+    birthday: "19980226",
+    address: "New York No. 2 Lake Park",
+  },
 ];
 
 class Login extends React.Component {
   state = {
-    data2: [],
+    selectedRowKeys: [],
+  };
+  onRowClick = (record) => {
+    const selectKey = [...this.state.selectedRowKeys];
+    if (selectKey.includes(record.id)) {
+      selectKey.splice(selectKey.indexOf(record.id), 1);
+    } else {
+      selectKey.push(record.id);
+    }
+    this.setState({
+      selectedRowKeys: selectKey,
+    });
   };
   render() {
+    const { selectedRowKeys } = this.state;
     return (
       <div style={{ width: "100%" }}>
         <Card title="基础表格">
-          <Table columns={columns} dataSource={data} pagination={false}></Table>
-        </Card>
-        <Card title="动态数据渲染表格">
           <Table
             columns={columns}
-            dataSource={this.state.data2}
+            dataSource={data}
             pagination={false}
+            rowKey="id"
+          ></Table>
+        </Card>
+        <Card title="表格操作">
+          <Table
+            rowKey="id"
+            rowSelection={{
+              selectedRowKeys,
+            }}
+            bordered
+            columns={columns}
+            dataSource={data}
+            pagination={false}
+            onRow={(record) => {
+              return {
+                onClick: () => {
+                  this.onRowClick(record);
+                },
+              };
+            }}
           ></Table>
         </Card>
       </div>
