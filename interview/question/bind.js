47c569bf147aedc8c00f8f2ccdb68237f014aed6
zzj <2461927976@qq.com>
2021-03-13 14:45:20 +0800

    简易版bind

--- interview/question/bind.js
+++ interview/question/bind.js
@@ -2,7 +2,7 @@
  * @Author: zzj
  * @Date: 2021-01-06 11:25:32
  * @LastEditors: zzj
- * @LastEditTime: 2021-01-06 13:44:36
+ * @LastEditTime: 2021-03-13 14:43:35
  * @Description:手写bind函数
  */
 
@@ -19,5 +19,14 @@ Function.prototype.myBind = function (context) {
       return new _this(...args, ...arguments);
     }
     return _this.apply(context, args.concat(...arguments));
+  };
+};
+
+Function.prototype.bind1 = function () {
+  const args = Array.prototype.slice.call(arguments);
+  const t = args.shift();
+  const self = this;
+  return function () {
+    self.apply(t, args);
   };
 };
