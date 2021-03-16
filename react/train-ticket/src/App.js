2c6e85aa3dcbb0bd0fb8b10131fe82a80ffe6c9a
zzj <2461927976@qq.com>
2021-03-16 14:10:45 +0800

    使用useState

--- react/train-ticket/src/App.js
+++ react/train-ticket/src/App.js
@@ -2,13 +2,27 @@
  * @Author: zzj
  * @Date: 2021-03-14 16:22:23
  * @LastEditors: zzj
- * @LastEditTime: 2021-03-15 22:38:00
+ * @LastEditTime: 2021-03-16 11:40:51
  * @Description:
  */
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">123</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      {count}
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        test
+      </button>
+    </div>
+  );
 }
 
 export default App;
