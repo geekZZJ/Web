f47dba908f08c4fa03661166415707d0a98118b4
zzj <2461927976@qq.com>
2021-03-20 15:05:00 +0800

    学习RAF

--- interview/question/RAF.js
+++ interview/question/RAF.js
@@ -1,32 +1,33 @@
-/**
- * @name: RAF
- * @author: 张中俊
- * @date: 2020/6/2 23:19
- * @description：RAF
- * @update: 2020/6/2 23:19
+/*
+ * @Author: zzj
+ * @Date: 2020-06-02 23:19:00
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-03-20 15:03:04
+ * @Description:
  */
-//3s把宽度从100px变成640px，即宽度增加540px
-//60帧/s，3s180帧，每次变化3px
 
-let curWidth = 100
-const maxWidth = 640
-const div1 = document.getElementById("div1")
+// 3s把宽度从100px变成640px，即宽度增加540px
+// 60帧/s，3s180帧，每次变化3px
 
-/*function animate() {
-  curWidth += 3
-  div1.style.width = curWidth + "px"
-  if (curWidth < maxWidth) {
-    setTimeout(animate, 16.7)  //自己控制时间
-  }
-}*/
+let curWidth = 100;
+const maxWidth = 640;
+const div1 = document.getElementById("div1");
 
-//RAF
+// function animate() {
+//   curWidth += 3;
+//   div1.style.width = curWidth + "px";
+//   if (curWidth < maxWidth) {
+//     setTimeout(animate, 16.7); //自己控制时间
+//   }
+// }
+
+// RAF
 function animate() {
-  curWidth += 3
-  div1.style.width = curWidth + "px"
+  curWidth += 3;
+  div1.style.width = curWidth + "px";
   if (curWidth < maxWidth) {
-    window.requestAnimationFrame(animate)  //时间不用自己控制
+    window.requestAnimationFrame(animate); //时间不用自己控制
   }
 }
 
-animate()
+animate();
--- interview/question/flat.js
+++ interview/question/flat.js
@@ -1,19 +1,19 @@
-/**
- * @name: flat
- * @author: 张中俊
- * @date: 2020/6/2 22:56
- * @description：flat
- * @update: 2020/6/2 22:56
+/*
+ * @Author: zzj
+ * @Date: 2020-06-02 22:56:47
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-03-19 22:14:43
+ * @Description:
  */
 function flat(arr) {
   //验证arr中，还有没有深层数组
-  const isDeep = arr.some(item => item instanceof Array)
+  const isDeep = arr.some((item) => item instanceof Array);
   if (!isDeep) {
-    return arr
+    return arr;
   }
-  const res = Array.prototype.concat.apply([], arr)
-  return flat(res)
+  const res = Array.prototype.concat.apply([], arr);
+  return flat(res);
 }
 
-const res = flat([1, 2, [3, 4, [7]], 5])
-console.log(res)
+const res = flat([1, 2, [3, 4, [7]], 5]);
+console.log(res);
