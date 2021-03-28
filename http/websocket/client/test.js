fd16f8f6b60674a9905d1a12b2a823c494c3d9b4
zzj <2461927976@qq.com>
2021-03-28 16:01:28 +0800

    心跳检测断线重连

--- http/websocket/client/test.js
+++ http/websocket/client/test.js
@@ -2,17 +2,17 @@
  * @Author: zzj
  * @Date: 2021-03-04 22:12:19
  * @LastEditors: zzj
- * @LastEditTime: 2021-03-04 22:29:58
+ * @LastEditTime: 2021-03-28 14:57:37
  * @Description:
  */
 const WebSocket = require("ws");
 
-const ws = new WebSocket("ws://127.0.0.1:3000");
+const ws = new WebSocket("ws://127.0.0.1:3000", { headers: { token: 123 } });
 
-ws.on("open", () => {
-  console.log("client is connected to server");
-  ws.send("client say hello to server");
-  ws.on("message", (msg) => {
-    console.log(msg);
-  });
-});
+// ws.on("open", () => {
+//   console.log("client is connected to server");
+//   ws.send("client say hello to server");
+//   ws.on("message", (msg) => {
+//     console.log(msg);
+//   });
+// });
--- http/websocket/server/index.js
+++ http/websocket/server/index.js
@@ -2,16 +2,21 @@
  * @Author: zzj
  * @Date: 2021-03-04 22:03:41
  * @LastEditors: zzj
- * @LastEditTime: 2021-03-20 22:35:06
+ * @LastEditTime: 2021-03-28 15:49:00
  * @Description:
  */
 const WebSocket = require("ws");
+const http = require("http");
+const server = http.createServer();
+const wss = new WebSocket.Server({ noServer: true });
+const jwt = require("jsonwebtoken");
 
-const wss = new WebSocket.Server({ port: 3000 });
+const timeInterval = 1000;
 
 let group = {};
 
 wss.on("connection", (ws) => {
+  ws.isAlive = true;
   console.log("one client is connected");
   // 接收客户端消息
   ws.on("message", (msg) => {
@@ -24,6 +29,32 @@ wss.on("connection", (ws) => {
       } else {
         group[ws.roomid]++;
       }
+    }
+    // 鉴权
+    if (msgObj.event === "auth") {
+      jwt.verify(msgObj.message, "secret", (err, decode) => {
+        if (err) {
+          ws.send(
+            JSON.stringify({ event: "noauth", message: "please auth again" })
+          );
+          return;
+        } else {
+          // 鉴权通过
+          console.log(decode);
+          ws.isAuth = true;
+          return;
+        }
+      });
+      return;
+    }
+    // 拦截非鉴权的请求
+    if (!ws.isAuth) {
+      return;
+    }
+    // 心跳监测
+    if (msgObj.event === "heartbeat" && msgObj.message === "pong") {
+      ws.isAlive = true;
+      return;
     }
     // 主动给客户端发送消息
     // ws.send(`server:${msg}`);
@@ -54,3 +85,39 @@ wss.on("connection", (ws) => {
     });
   });
 });
+
+server.on("upgrade", function upgrade(request, socket, head) {
+  console.log("upgrade -> request", request.headers);
+  // This function is not defined on purpose. Implement it with your own logic.
+  // authenticate(request, (err, client) => {
+  //   if (err || !client) {
+  //     socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+  //     socket.destroy();
+  //     return;
+  //   }
+
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit("connection", ws, request);
+  });
+  // });
+});
+
+server.listen(3000);
+
+setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (!ws.isAlive) {
+      group[ws.roomid]--;
+      return ws.terminate();
+    }
+    // 主动发送心跳监测请求
+    ws.isAlive = false;
+    ws.send(
+      JSON.stringify({
+        event: "heartbeat",
+        message: "ping",
+        num: group[ws.roomid],
+      })
+    );
+  });
+}, timeInterval);
