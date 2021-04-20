a280c81855eeb03de7c1aaf1697400ef73086536
zzj <2461927976@qq.com>
2021-04-20 16:56:02 +0800

    增加城市

--- nodejs/train-mock/index.js
+++ nodejs/train-mock/index.js
@@ -2,7 +2,7 @@
  * @Author: zzj
  * @Date: 2021-04-04 12:10:49
  * @LastEditors: zzj
- * @LastEditTime: 2021-04-19 21:57:27
+ * @LastEditTime: 2021-04-20 16:49:15
  * @Description:
  */
 const express = require("express");
@@ -27,7 +27,28 @@ app.get("/rest/cities", (request, response) => {
     cityList: [
       { title: "A", cities: [{ name: "阿尔山" }, { name: "阿克苏" }] },
       { title: "B", cities: [{ name: "包头" }, { name: "北海" }] },
-      { title: "C", cities: [{ name: "长春" }, { name: "长沙" }] },
+      {
+        title: "C",
+        cities: [{ name: "长春" }, { name: "长沙" }, { name: "长治" }],
+      },
+      {
+        title: "D",
+        cities: [
+          { name: "大理" },
+          { name: "大连" },
+          { name: "大庆" },
+          { name: "大同" },
+          { name: "达县" },
+        ],
+      },
+      {
+        title: "E",
+        cities: [{ name: "鄂尔多斯" }],
+      },
+      {
+        title: "F",
+        cities: [{ name: "福州" }, { name: "佛山" }],
+      },
     ],
     msg: "hello world",
   });
