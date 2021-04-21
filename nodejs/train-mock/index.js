2e1ad95c056e3457e1bdf10311418bcc70bc33c8
zzj <2461927976@qq.com>
2021-04-21 10:08:40 +0800

    增加搜索接口

--- nodejs/train-mock/index.js
+++ nodejs/train-mock/index.js
@@ -2,12 +2,31 @@
  * @Author: zzj
  * @Date: 2021-04-04 12:10:49
  * @LastEditors: zzj
- * @LastEditTime: 2021-04-20 16:49:15
+ * @LastEditTime: 2021-04-21 10:06:48
  * @Description:
  */
 const express = require("express");
-
 const app = express();
+
+//设置跨域访问
+app.all("*", function (req, res, next) {
+  // 设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  // 允许的header类型
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  // 跨域允许的请求方式
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  // 可以带cookies
+  res.header("Access-Control-Allow-Credentials", true);
+  if (req.method == "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.get("/", (request, response) => {
   response.status(200);
@@ -51,6 +70,13 @@ app.get("/rest/cities", (request, response) => {
       },
     ],
     msg: "hello world",
+  });
+});
+
+app.get("/rest/search", (request, response) => {
+  response.json({
+    sKey: "a",
+    result: [{ display: "阿克苏" }, { display: "阿尔山" }],
   });
 });
 
