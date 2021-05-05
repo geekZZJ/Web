6e89a79c2864418b9255cfa640eba1d6ccb5bc47
zzj <2461927976@qq.com>
2021-05-05 20:41:18 +0800

    URL解析与数据请求

--- react/train-ticket/src/query/App.js
+++ react/train-ticket/src/query/App.js
@@ -2,20 +2,144 @@
  * @Author: zzj
  * @Date: 2021-04-03 14:52:13
  * @LastEditors: zzj
- * @LastEditTime: 2021-04-24 16:33:38
+ * @LastEditTime: 2021-05-05 18:41:20
  * @Description:
  */
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { connect } from "react-redux";
+import URI from "urijs";
 import "./App.css";
 
 import Nav from "../common/Nav";
 import List from "./List";
 import Bottom from "./Bottom";
+import Header from "../common/Header";
+
+import {
+  setFrom,
+  setTo,
+  setDepartDate,
+  setHighSpeed,
+  setSearchParsed,
+  setTrainList,
+  setTicketTypes,
+  setTrainTypes,
+  setDepartStations,
+  setArriveStations,
+} from "./actions";
+import dayjs from "dayjs";
+import { h0 } from "../common/fp";
+import axios from "axios";
 
 function App(props) {
+  const {
+    from,
+    to,
+    departDate,
+    highSpeed,
+    dispatch,
+    searchParsed,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  } = props;
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+    const { from, to, date, highSpeed } = queries;
+    dispatch(setFrom(from));
+    dispatch(setTo(to));
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+    dispatch(setHighSpeed(highSpeed === "true"));
+    dispatch(setSearchParsed(true));
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    if (!searchParsed) {
+      return;
+    }
+    const url = new URI("/rest/query")
+      .setSearch("from", from)
+      .setSearch("to", to)
+      .setSearch("date", dayjs(departDate).format("YYYY-MM-DD"))
+      .setSearch("highSpeed", highSpeed)
+      .setSearch("orderType", orderType)
+      .setSearch("onlyTickets", onlyTickets)
+      .setSearch("checkedTicketTypes", Object.keys(checkedTicketTypes).join())
+      .setSearch("checkedTrainTypes", Object.keys(checkedTrainTypes).join())
+      .setSearch(
+        "checkedDepartStations",
+        Object.keys(checkedDepartStations).join()
+      )
+      .setSearch(
+        "checkedArriveStations",
+        Object.keys(checkedArriveStations).join()
+      )
+      .setSearch("departTimeStart", departTimeStart)
+      .setSearch("departTimeEnd", departTimeEnd)
+      .setSearch("arriveTimeStart", arriveTimeStart)
+      .setSearch("arriveTimeEnd", arriveTimeEnd)
+      .toString();
+
+    async function fetchData() {
+      try {
+        const res = await axios.get(url);
+        const {
+          dataMap: {
+            directTrainInfo: {
+              trains,
+              filter: { ticketType, trainType, depStation, arrStation },
+            },
+          },
+        } = res.data;
+        dispatch(setTrainList(trains));
+        dispatch(setTicketTypes(ticketType));
+        dispatch(setTrainTypes(trainType));
+        dispatch(setDepartStations(depStation));
+        dispatch(setArriveStations(arrStation));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+    // eslint-disable-next-line
+  }, [
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  ]);
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  if (!searchParsed) {
+    return null;
+  }
+
   return (
     <div>
+      <div className="header-wrapper">
+        <Header title={`${from} - ${to}`} onBack={onBack}></Header>
+      </div>
       <Nav></Nav>
       <List></List>
       <Bottom></Bottom>
