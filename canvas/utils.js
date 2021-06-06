0006578f043cad6375d1210e3bd04690f1e24e51
zzj <2461927976@qq.com>
2021-06-06 17:53:20 +0800

    删除ajax文件夹

--- canvas/utils.js
+++ canvas/utils.js
@@ -1,28 +1,36 @@
+/*
+ * @Author: zzj
+ * @Date: 2019-01-05 22:29:08
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-05-20 22:10:07
+ * @Description:
+ */
 let c = {};
 
+// 获取鼠标在元素上的坐标
 c.getOffset = function (ele) {
-  let mouse = {x: 0, y: 0};
+  let mouse = { x: 0, y: 0 };
   ele.addEventListener("mousemove", function (e) {
-    let {x, y} = c.eventWrapper(e);
+    let { x, y } = c.eventWrapper(e);
     mouse.x = x;
     mouse.y = y;
   });
   return mouse;
 };
 
+// 坐标系转换
 c.eventWrapper = function (ev) {
-  let {pageX, pageY, target} = ev;
-  let {left, top} = target.getBoundingClientRect();
-  return {x: pageX - left, y: pageY - top};
+  let { pageX, pageY, target } = ev;
+  let { left, top } = target.getBoundingClientRect();
+  return { x: pageX - left, y: pageY - top };
 };
-
 
 //角度转弧度
 c.toRad = function (angle) {
-  return angle * Math.PI / 180;
+  return (angle * Math.PI) / 180;
 };
 
-
+// 弧度转角度
 c.toAngle = function (rad) {
-  return rad * 180 / Math.PI;
+  return (rad * 180) / Math.PI;
 };
