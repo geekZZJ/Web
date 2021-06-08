05088b26109a78ca55823ee0db65f0f36e10872b
zzj <2461927976@qq.com>
2021-06-08 11:18:28 +0800

    初始化项目

---
+++ vuejs/my-mpvue/src/App.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: zzj
+ * @Date: 2021-06-06 22:07:50
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-06-07 22:27:54
+ * @Description: 
+-->
+<script>
+export default {
+  created() {},
+};
+</script>
+
+<style>
+</style>
---
+++ vuejs/my-mpvue/src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: zzj
+ * @Date: 2021-06-07 22:29:25
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-06-08 10:01:33
+ * @Description: 
+-->
+<template>
+  <div class="test">hello mpvue</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang='scss' scoped>
+$color: red;
+.test {
+  color: $color;
+}
+</style>
