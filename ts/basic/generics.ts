f276399467520ad64afdcc4a24e3d50a2818aaf3
zzj <2461927976@qq.com>
2022-06-05 13:10:17 +0800

    约束范型

--- ts/basic/generics.ts
+++ ts/basic/generics.ts
@@ -9,4 +9,23 @@ function swap<T, U>(tuple: [T, U]): [U, T] {
   return [tuple[1], tuple[0]];
 }
 
-const res = swap([123, '456']); 
+const res = swap([123, "456"]);
+
+function echoWithArr<T>(arg: T[]): T[] {
+  console.log(arg.length);
+  return arg;
+}
+const arrs = echoWithArr([1, 2, 3]);
+
+interface IWithLength {
+  length: number;
+}
+
+function echoWithLength<T extends IWithLength>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+
+const str = echoWithLength("123");
+const obj = echoWithLength({ length: 123 });
+const arr2 = echoWithLength([1, 2, 3]);
