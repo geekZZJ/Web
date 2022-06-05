c2b82829d0b4c507fd95d703153daf04e032c706
zzj <2461927976@qq.com>
2022-06-05 22:59:36 +0800

    范型在类和接口中的使用

--- ts/basic/generics.ts
+++ ts/basic/generics.ts
@@ -29,3 +29,33 @@ function echoWithLength<T extends IWithLength>(arg: T): T {
 const str = echoWithLength("123");
 const obj = echoWithLength({ length: 123 });
 const arr2 = echoWithLength([1, 2, 3]);
+
+class Queue<T> {
+  private data = [];
+  push(item: T) {
+    return this.data.push(item);
+  }
+
+  pop(): T {
+    return this.data.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.push(1);
+console.log(queue.pop().toFixed());
+
+interface KeyPair<T, U> {
+  key: T;
+  value: U;
+}
+
+let kp1: KeyPair<number, string> = {
+  key: 1,
+  value: "string",
+};
+
+let kp2: KeyPair<string, number> = {
+  key: "string",
+  value: 2,
+};
