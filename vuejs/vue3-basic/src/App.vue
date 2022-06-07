f66a5f912cf4487636f190c1a2d8743702908fb3
zzj <2461927976@qq.com>
2022-06-08 00:23:57 +0800

    ref使用

--- vuejs/vue3-basic/src/App.vue
+++ vuejs/vue3-basic/src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>{{ count }}</div>
+  <div>{{ double }}</div>
+  <button @click="increase">+1</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref, computed } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  setup() {
+    const count = ref(0);
+    const double = computed(() => {
+      return count.value * 2;
+    });
+    const increase = () => count.value++;
+    return {
+      count,
+      increase,
+      double
+    };
+  },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
